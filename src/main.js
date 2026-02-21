a643190c9a0f3b5635c22ed90371db781936d9bb
Sohibjon Zoirov <sohibjonzoirov@gmail.com>
2026-02-21 18:01:32 +0500

    Sayt to'g'irlandi

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-navigator.serviceWorker.register('/service-worker.js')
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(() => console.log("Service Worker registered"))
+      .catch(err => console.log("SW error:", err))
+  })
+}
 
 createApp(App).use(router).mount("#app")
